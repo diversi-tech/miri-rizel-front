a6b24e05d80d0a3a0ec2a1edbf98197d812718ec
rachelYaffe <r0548431830@gmail.com>
2024-06-17 13:32:40 +0300

    Add LoginComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorHandlingComponent } from './error-handling/error-handling.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorHandlingComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
