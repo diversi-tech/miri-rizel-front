037cf70a9e9075db9bbbd823b66f2babe665fe2b
ayalavardi <ayala.amt@gmail.com>
2024-06-27 09:55:31 +0300

    A generic table almost completed

--- src/app/Components/generic-bourd/generic-bourd.component.ts
+++ src/app/Components/generic-bourd/generic-bourd.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { Table } from 'primeng/table';
 
 interface Column {
   field: string;
@@ -16,9 +17,28 @@ export class GenericBourdComponent implements OnInit, OnChanges {
   @Input() data: any[] = [];
   @Input() loading: boolean = false;
   @Input() globalFilterFields: string[] = [];
-  columns: Column[] = [];
+  @Input() positionData: any[] = [];
+  @Input() objData: any[] = [];
+  @Input() objFields: string[] = [];
+  @Input() col$types: any = {};
+  @Input() popTable!: boolean;
+  @Output() edit = new EventEmitter<any>();
+  @Output() delete = new EventEmitter<any>();
+  @Output() dataUpdated = new EventEmitter<any>();
 
+  @ViewChild('dt') dt!: Table;
+
+  columns: Column[] = [];
+  statuses = [
+    { label: 'InProgress', value: 'InProgress' },
+    { label: 'Complited', value: 'Complited' },
+    { label: 'Beginning', value: 'Beginning' },
+
+  ];
   ngOnInit() {
+    if ( this.data === undefined||(this.objData.length>0&&this.objFields==null)) {
+      throw new Error('The data input is required and must be provided.');
+    }
     this.generateColumns();
   }
 
@@ -27,38 +47,119 @@ export class GenericBourdComponent implements OnInit, OnChanges {
       this.generateColumns();
     }
   }
+  onEdit(rowData: any) {
+    this.edit.emit(rowData);
+  }
 
+  onDelete(rowData: any) {
+    this.delete.emit(rowData);
+  }
   generateColumns() {
     if (this.data.length === 0) {
       this.columns = [];
       return;
     }
-    
     const firstItem = this.data[0];
     if (!firstItem) return;
-    
     this.columns = [];
-    for (const key in firstItem) {
-      console.log(firstItem);
-      
-      if (firstItem.hasOwnProperty(key)) {
-        let d=new Date(firstItem[key])
-        let s=!isNaN(d.getTime())?'date':'text'
-        
-        this.columns.push({
-          field: key,
-          header: this.capitalizeFirstLetter(key),
-          sortable: true, // אפשר לשנות בהתאם לצורך
-          filterType: s
-        });
-      }
+    Object.keys(this.col$types).forEach((key) => {
+      this.columns.push({
+        field: key,
+        header: this.capitalizeFirstLetter(key),
+        sortable: true,
+        filterType: this.col$types[key]
+      });
+    })
+    if(this.popTable==true)
+      this.columns.push({
+        field: 'popTable',
+        header: 'Show ღ',
+        sortable: false,
+        filterType: 'popTable'
+      });
+    this.columns.push({
+      field: 'edit',
+      header: 'Edit',
+      sortable: false,
+      filterType: 'edit'
+    });
+    this.columns.push({
+      field: 'delete',
+      header:'Delete',
+      sortable: false,
+      filterType: 'delete'
+    });
+
+  }
+
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'a':
+        return 'danger';
+
+      case 'Complited':
+        return 'success';
+
+      case 'Beginning':
+        return 'info';
+
+      case 'InProgress':
+        return 'warning';
+
+      case 'renewal':
+        return 'null';
+
+      default:
+        return 'null';
+
     }
   }
 
-  capitalizeFirstLetter(string: string) {
+  capitalizeFirstLetter(field: string) {
+   let string= field.replace(/([A-Z])/g, ' $1').toLowerCase();
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-  getTypeOfCol(col:Column){
-   return col.filterType
+  getTypeOfCol(col: Column, i: number) {
+    return col.filterType
   }
+  getpositionData(i: number) {
+    let index: number = 0
+    for (let c = 0; c < i; c++)
+      if (this.columns[c].filterType == 'priority')
+        index++;
+    return this.positionData[index]
+  }
+  getObjData(i: number) {
+    let index: number = 0
+    for (let c = 0; c < i; c++)
+      if (this.columns[c].filterType == 'obj')
+        index++;
+    return this.objData[index]
+
+  }
+  getobjFields(i: number): string {
+    let index: number = 0
+    for (let c = 0; c < i; c++)
+      if (this.columns[c].filterType == 'obj')
+        index++;
+    return this.objFields[index]
+
+  }
+
+  filterGlobal(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (this.dt) {
+      this.dt.filterGlobal(input.value, 'contains');
+    }
+  }
+
+  getDataForPopTable(obj: any) {
+    this.dataUpdated.emit(obj)
+  }
+  
+  PopTable(data: any) {
+    console.log("hooooooo", data);
+  }
+
 }
--- src/app/Model/Task.ts
+++ src/app/Model/Task.ts
@@ -8,6 +8,6 @@ export interface Task {
     priority?: string,
     dueDate?: Date,
     assignedTo?: User,
-    projectId?: Project,
+    project?: Project,
     createdDate?: Date
 }
