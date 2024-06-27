c606418ef7b2ff3d3eb1fcf05595962f59066585
sarihaiman <sari9741497@gmail.com>
2024-06-27 16:48:47 +0300

    add lead crud

--- src/app/services/lead.service.ts
+++ src/app/services/lead.service.ts
@@ -7,12 +7,31 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class LeadService {
-  constructor(private http: HttpClient) {}
-  private url = 'https://localhost:7141/Lead/';
+  private apiUrl = 'https://localhost:7141/Lead';
+
+  constructor(private http: HttpClient) { }
+
+  GetLeadById(id: Number): Observable<Lead> {
+    const url = `https://localhost:7141/Lead/GetById?id=${id}`;
+    return this.http.get<Lead>(url); // Specify the type of data as Lead
+  }
 
   addLead(lead: Lead): Observable<Lead> {
-    console.log("hello service");
-    
-    return this.http.post<Lead>(this.url + 'Add', lead);
+    return this.http.post<Lead>(`${this.apiUrl}/Add`, lead);
+  }
+
+  getAllLeads(): Observable<Lead[]> {
+    return this.http.get<Lead[]>(`${this.apiUrl}/ReadAll`);
+  }
+
+  editLead(lead: Lead,id:Number): Observable<Lead>  {
+    console.log(lead.leadId);
+    lead.leadId = id;
+    console.log(lead.leadId);
+    return this.http.put<Lead>(`${this.apiUrl}/Update`, lead);
+  }
+
+  deleteLead(id: Number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/Delete/?id=${id}`);
   }
 }
