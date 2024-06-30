bbd03cb3faf80bcc55e646b66e143237f36766da
sarihaiman <sari9741497@gmail.com>
2024-06-30 09:18:48 +0300

    add service lead

--- src/app/services/lead.service.ts
+++ src/app/services/lead.service.ts
@@ -1,53 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Lead } from '../Model/Lead';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LeadService {
-  constructor(private http: HttpClient) {}
-  private url = 'https://localhost:7141/Lead/';
+  private apiUrl = 'https://localhost:7141/Lead';
 
-  addLead(lead: Lead): Observable<Lead> {
-    return this.http.post<Lead>(this.url + 'Add', lead);
+  constructor(private http: HttpClient) { }
+
+  GetLeadById(id: Number): Observable<Lead> {
+    const url = `https://localhost:7141/Lead/GetById?id=${id}`;
+    return this.http.get<Lead>(url); // Specify the type of data as Lead
   }
 
-  // sortByName(name: string): Observable<Lead[]> {
-  //   return this.http
-  //   .get<Lead[]>(this.url)
-  //   .pipe(
-  //     map((leads: Lead[]) =>
-  //       leads.filter(
-  //         (lead) =>
-  //           lead.firstName == name ||
-  //           lead.lastName == name ||
-  //           `${lead.firstName} ${lead.lastName}` == name
-  //       )
-  //     )
-  //   );
-  // }
+  addLead(lead: Lead): Observable<Lead> {
+    return this.http.post<Lead>(`${this.apiUrl}/Add`, lead);
+  }
 
-  // sortByCreatedDate(createdDate: Date) {
-  //   return this.http
-  //   .get<Lead[]>(this.url)
-  //   .pipe(
-  //     map((leads: Lead[]) =>
-  //       leads.filter((lead) => lead.createdDate === createdDate)
-  //     )
-  //   );
-  // }
+  getAllLeads(): Observable<Lead[]> {
+    return this.http.get<Lead[]>(`${this.apiUrl}/ReadAll`);
+  }
 
-  // sortByBusinessName(businessName: string) {
-  //   return this.http
-  //   .get<Lead[]>(this.url)
-  //   .pipe(
-  //     map((leads: Lead[]) =>
-  //       leads.filter((lead) => lead.businessName === businessName)
-  //     )
-  //   );
-  // }
+  editLead(lead: Lead,id:Number): Observable<Lead>  {
+    console.log(lead.leadId);
+    lead.leadId = id;
+    console.log(lead.leadId);
+    return this.http.put<Lead>(`${this.apiUrl}/Update`, lead);
+  }
 
-
+  deleteLead(id: Number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/Delete/?id=${id}`);
+  }
 }
