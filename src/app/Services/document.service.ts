daf5b5cdef7bc0ce13f45a89c700c27e6a7b36c8
rachelYeffe <r0548431830@gmail.com>
2024-07-11 00:15:43 +0300

    chnge url-drive

--- src/app/Services/document.service.ts
+++ src/app/Services/document.service.ts
@@ -6,16 +6,18 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DocumentService {
-  private baseUrl = 'https://localhost:7141/api/FileUpload';  // שנה לכתובת ה-API שלך
+  private baseUrl = 'https://localhost:7141/api';
 
   constructor(private http: HttpClient) { }
   upFile(file: FormData): Observable<string> {
-    return this.http.post<string>(`${this.baseUrl}/upload`, file);
+    return this.http.post<string>(`${this.baseUrl}/FileUpload/upload`, file);
   }
-  addDocument(document: FormData):Observable<boolean> {
-    return this.http.post<boolean>(`${this.baseUrl}/upload`, document);
+  addDocument(document: Document):Observable<boolean> {
+    console.log(document);
+    
+    return this.http.post<boolean>(`${this.baseUrl}/Document`, document);
   }
   getFiles(): Observable<any> {
-    return this.http.get<any>(`${this.baseUrl}/list`);
+    return this.http.get<any>(`${this.baseUrl}/FileUpload/list`);
   }
 }
