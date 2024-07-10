421ab66bc8300fec53efa687ce6dc8f616c52e0a
rachelYeffe <r0548431830@gmail.com>
2024-07-11 00:12:05 +0300

    chang url customers

--- src/app/Services/customers.service.ts
+++ src/app/Services/customers.service.ts
@@ -14,21 +14,21 @@ export class CustomersService {
     return this.http.get<Customer[]>("https://localhost:7141/Customer");
   }
   GetCustomerById(customerId:number): Observable<Customer> {
-    return this.http.get<Customer>(`https://localhost:7141/Customer/getCustomerById/?custometId=${customerId}` );
+    return this.http.get<Customer>(`https://localhost:7141/Customer/GetById?custometId=${customerId}` );
   }
   AddNewCustomer(newCustomer:any):Observable<Customer>{
-    return this.http.post<Customer>(`https://localhost:7141/Customer/addNewCustomer/`,newCustomer);
+    return this.http.post<Customer>(`https://localhost:7141/Customer/`,newCustomer);
   } 
   EditCustomer(editCustomer:Customer):Observable<boolean>{
    
   
     editCustomer.status=editCustomer.status as StatusCodeUser;    
-    return this.http.put<boolean>(`https://localhost:7141/Customer/editCustomer/`,editCustomer);
+    return this.http.put<boolean>(`https://localhost:7141/Customer/`,editCustomer);
   } 
   
   DeleteCustomer(customerId:number):Observable<boolean>{
     
-    return this.http.delete<boolean>(`https://localhost:7141/Customer/DeleteCustomer?customerId=${customerId}` );
+    return this.http.delete<boolean>(`https://localhost:7141/Customer?customerId=${customerId}` );
   } 
   GetAllStatusUser():Observable<StatusCodeUser[]>{
     return this.http.get<StatusCodeUser[]>("https://localhost:7141/Customer/GetAllStatus");
