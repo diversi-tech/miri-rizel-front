1942f366c09120ee89299ea7cd599a512526e32a
feigi <Feigi7063@gmail.com>
2024-07-15 16:17:48 +0300

    add components

---
+++ src/app/Components/worker-component/worker-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-worker-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './worker-component.component.html',
+  styleUrl: './worker-component.component.css'
+})
+export class WorkerComponentComponent {
+
+}
