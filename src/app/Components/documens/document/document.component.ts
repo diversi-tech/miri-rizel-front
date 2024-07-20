2c96fea99321c1657d8fe6da3836049ad9233a9e
rachelYeffe <r0548431830@gmail.com>
2024-07-21 01:29:08 +0300

    add translate for document

--- src/app/Components/documens/document/document.component.ts
+++ src/app/Components/documens/document/document.component.ts
@@ -5,13 +5,14 @@ import { ValidatorsService } from '@app/Services/validators.service';
 import Swal from 'sweetalert2';
 import { NgIf } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-document',
     templateUrl: './document.component.html',
     styleUrls: ['./document.component.css'],
     standalone: true,
-    imports: [FormsModule, ReactiveFormsModule, NgIf,InputTextModule]
+    imports: [FormsModule, ReactiveFormsModule,NgIf,InputTextModule,TranslateModule]
 })
 export class DocumentComponent implements OnInit {
   file!: File;
