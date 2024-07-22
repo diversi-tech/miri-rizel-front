7713fe240f86da6332a737263ca81402e413be60
ayalavardi <ayala.amt@gmail.com>
2024-07-22 10:22:56 +0300

    homePage

--- src/app/Components/home-page/home-page.component.ts
+++ src/app/Components/home-page/home-page.component.ts
@@ -33,7 +33,7 @@ import { GenericBourdComponent } from '../generic-bourd/generic-bourd.component'
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports:  [GenericBourdComponent,MatIconModule, MatButtonModule, MatRippleModule, MatMenuModule, MatTabsModule, MatButtonToggleModule, NgApexchartsModule, NgFor, NgIf, MatTableModule, NgClass, CurrencyPipe],
+  imports: [GenericBourdComponent, MatIconModule, MatButtonModule, MatRippleModule, MatMenuModule, MatTabsModule, MatButtonToggleModule, NgApexchartsModule, NgFor, NgIf, MatTableModule, NgClass, CurrencyPipe],
 })
 export class HomePageComponent implements OnInit, OnDestroy {
 
@@ -51,7 +51,7 @@ export class HomePageComponent implements OnInit, OnDestroy {
 
   currentEmail: string = localStorage.getItem("email") ?? 'מסתננת';
   componentType!: Type<any>;
-  
+
   @ViewChild('popupContainer', { read: ViewContainerRef }) popupContainer!: ViewContainerRef;
   @ViewChild(GenericBourdComponent) genericBourd!: GenericBourdComponent;
 
@@ -131,8 +131,8 @@ export class HomePageComponent implements OnInit, OnDestroy {
         console.error('Error fetching project:', error);
         this.loading = true;
       })
-    }
-  
+  }
+
   calculateStats() {
     this.activeCustomers = this.customers.filter(customer => customer.status.description === 'Active').length;
     this.inactiveCustomers = this.customers.filter(customer => customer.status.description === 'Inactive').length;
@@ -146,8 +146,8 @@ export class HomePageComponent implements OnInit, OnDestroy {
       chart: {
         fontFamily: 'inherit',
         foreColor: 'inherit',
-        height: 400, // גובה מותאם אישית בפיקסלים
-        width: '100%', // רוחב מלא של ה-container
+        height: 400, 
+        width: '100%', 
         type: 'polarArea',
         toolbar: {
           show: false,
@@ -183,7 +183,7 @@ export class HomePageComponent implements OnInit, OnDestroy {
       stroke: {
         width: 2,
       },
-      colors: ['#5CFFAC', '#A0A0A0', '#7F7F7F'], // Custom colors for your chart
+      colors: ['#5CFFAC', '#A0A0A0', '#7F7F7F'],
       tooltip: {
         followCursor: true,
         theme: 'dark',
@@ -229,7 +229,7 @@ export class HomePageComponent implements OnInit, OnDestroy {
       let objData = [this.projects];
       let objFields = ['name'];
       if (this.genericBourd) {
-        this.genericBourd.PopTable(taskFilter, loading, col$types, objData, objFields, positionD,"1000px");
+        this.genericBourd.PopTable(taskFilter, loading, col$types, objData, objFields, positionD, "1000px");
       } else {
         console.error('genericBourd is not initialized');
       }
@@ -243,32 +243,8 @@ export class HomePageComponent implements OnInit, OnDestroy {
       });
     }
   }
-  // routing(list: any) {    
-  //   this.loading = false;
-  //   let taskFilter: Task[] = list
-  //   console.log(taskFilter);
-  //   if (taskFilter.length != 0) {
-  //     let loading: boolean = true;
-  //     let col$types = { 'title': 'text' };
-  //     let positionD = [this.statuses];
-  //     let objData = [this.projects];
-  //     let objFields = ['name'];
-  //     console.log("taskFilter, loading, col$types, objData, objFields, positionD");
-  //     console.log(taskFilter, loading, col$types, objData, objFields, positionD);
-      
-  //     this.genericBourd.PopTable(taskFilter, loading, col$types, objData, objFields, positionD);
-  //   } else {
-  //     // this.translate.get(['Close', 'no tasks']).subscribe(translations => {
-  //       Swal.fire({
-  //         text: 'no tasks',
-  //         showCancelButton: false,
-  //         showCloseButton: true,
-  //         confirmButtonColor: "#d33",
-  //         confirmButtonText: 'Close'
-  //       });
-  //     };
-  //   }
-    
+
+
   addLead() {
     this.componentType = AddLeadComponent;
     this.popUpAddOrEdit("Add Lead");
@@ -303,13 +279,18 @@ export class HomePageComponent implements OnInit, OnDestroy {
     return this.tasks
   }
   getTodoTasks(): Task[] {
+    if(this.tasks)
     // return this.tasks.filter(task => task.status?.description === "TO DO");
     return this.tasks.filter(task => task.status?.description === "Not Started");
+  return []
   }
   getTodoTasksByUser(): Task[] {
     // return this.tasks.filter(task => task.status?.description === "TO DO");
-    return this.tasks.filter(task => task.status?.description === "Not Started" && task.assignedTo?.email == this.user.email);
+    if (this.tasks)
+      return this.tasks.filter(task => task.status?.description === "Not Started" && task.assignedTo?.email == this.user.email);
+    return []
   }
+
   getTodoTasksFromToday(): Task[] {
     const today = new Date().toDateString();
     return this.getTodoTasks().filter(task => {
@@ -322,11 +303,15 @@ export class HomePageComponent implements OnInit, OnDestroy {
 
   getInProgressTasks(): Task[] {
     // return this.tasks.filter(task => task.status?.description === "In PROGRESS");
+    if(this.tasks)
     return this.tasks.filter(task => task.status?.description === "In Progress");
+  return[]
   }
   getInProgressTasksByUser(): Task[] {
-    // return this.tasks.filter(task => task.status?.description === "In PROGRESS");
-    return this.tasks.filter(task => task.status?.description === "In Progress" && task.assignedTo?.email == this.user.email);
+    if (this.tasks)
+      // return this.tasks.filter(task => task.status?.description === "In PROGRESS");
+      return this.tasks.filter(task => task.status?.description === "In Progress" && task.assignedTo?.email == this.user.email);
+    return []
   }
   getInProgressTasksFromToday(): Task[] {
     const today = new Date().toDateString();
@@ -341,15 +326,18 @@ export class HomePageComponent implements OnInit, OnDestroy {
 
   getTasksDueToday(): Task[] {
     const today = new Date().toDateString();
+    if(this.tasks){
     return this.tasks.filter(task => {
       if (task.dueDate) {
         return new Date(task.dueDate).toDateString() === today;
       }
       return false;
-    })
+    })}
+  return []
   }
   getTasksDueTodayAndClosed(): Task[] {
     const today = new Date().toDateString();
+    if(this.tasks)
     return this.tasks.filter(task => {
       if (task.dueDate) {
         // return new Date(task.dueDate).toDateString() === today&&task.status?.description=="DONE";
@@ -357,11 +345,11 @@ export class HomePageComponent implements OnInit, OnDestroy {
       }
       return false;
     })
+    return []
   }
   getTasksOverdue(): Task[] {
     const now = new Date();
-    console.log('Current date and time:', now.toISOString());
-
+    if(this.tasks)
     return this.tasks.filter(task => {
       if (task.dueDate) {
         const dueDate = new Date(task.dueDate);
@@ -370,97 +358,36 @@ export class HomePageComponent implements OnInit, OnDestroy {
       }
       return false;
     });
+    return[]
   }
   getTasksOverdueFromYesterday(): Task[] {
     const yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 1);
     const yesterdayDateString = yesterday.toDateString();
-
-    // מסנן את המשימות שתאריך ההגשה שלהן היה אתמול
+    if(this.tasks)
     return this.tasks.filter(task => {
       if (task.dueDate) {
         return new Date(task.dueDate).toDateString() === yesterdayDateString;
       }
       return false;
     });
+    return[]
   }
 
-  // ngOnInit(): void {
-  //   // Get the data
-  //   this._projectService.data$
-  //     .pipe(takeUntil(this._unsubscribeAll))
-  //     .subscribe((data) => {
-  //       // Store the data
-  //       this.data = data;
-
-  //       // Prepare the chart data
-  //       this._prepareChartData();
-  //     });
-  //   if (!window['Apex']) {
-  //     window['Apex'] = {};
-  //   }
-  //   window['Apex'].chart = {
-  //     events: {
-  //       mounted: (chart: any, options?: any): void => {
-  //         this._fixSvgFill(chart.el);
-  //       },
-  //       updated: (chart: any, options?: any): void => {
-  //         this._fixSvgFill(chart.el);
-  //       },
-  //     },
-  //   };
-  // }
-
-  /**
-   * On destroy
-   */
+ 
   ngOnDestroy(): void {
-    // Unsubscribe from all subscriptions
     this._unsubscribeAll.next(null);
     this._unsubscribeAll.complete();
   }
 
-  // -----------------------------------------------------------------------------------------------------
-  // @ Public methods
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * Track by function for ngFor loops
-   *
-   * @param index
-   * @param item
-   */
+  
   trackByFn(index: number, item: any): any {
     return item.id || index;
   }
 
-  // -----------------------------------------------------------------------------------------------------
-  // @ Private methods
-  // -----------------------------------------------------------------------------------------------------
-
-  /**
-   * Fix the SVG fill references. This fix must be applied to all ApexCharts
-   * charts in order to fix 'black color on gradient fills on certain browsers'
-   * issue caused by the '<base>' tag.
-   *
-   * Fix based on https://gist.github.com/Kamshak/c84cdc175209d1a30f711abd6a81d472
-   *
-   * @param element
-   * @private
-   */
   private _fixSvgFill(element: Element): void {
-    // Current URL
     const currentURL = this.router.url;
-
-    // 1. Find all elements with 'fill' attribute within the element
-    // 2. Filter out the ones that doesn't have cross reference so we only left with the ones that use the 'url(#id)' syntax
-    // 3. Insert the 'currentURL' at the front of the 'fill' attribute value
     Array.from(element.querySelectorAll('*[fill]'))
-      // .filter(el => el.getAttribute('fill').indexOf('url(') !== -1)
-      // .forEach((el) => {
-      //   const attrVal = el.getAttribute('fill');
-      //   el.setAttribute('fill', `url(${currentURL}${attrVal.slice(attrVal.indexOf('#'))})`);
-      // });
       .filter(el => {
         const fillAttr = el.getAttribute('fill');
         return fillAttr && fillAttr.indexOf('url(') !== -1;
@@ -468,314 +395,8 @@ export class HomePageComponent implements OnInit, OnDestroy {
 
   }
 
-  /**
-   * Prepare the chart data from the data
-   *
-   * @private
-   */
+ 
   private _prepareChartData(): void {
-    // Github issues
-    console.log("_prepareChartData");
-
-    // this.chartGithubIssues = {
-    //   chart: {
-    //     fontFamily: 'inherit',
-    //     foreColor: 'inherit',
-    //     height: '100%',
-    //     type: 'line',
-    //     toolbar: {
-    //       show: false,
-    //     },
-    //     zoom: {
-    //       enabled: false,
-    //     },
-    //   },
-    //   colors: ['#64748B', '#94A3B8'],
-    //   dataLabels: {
-    //     enabled: true,
-    //     enabledOnSeries: [0],
-    //     background: {
-    //       borderWidth: 0,
-    //     },
-    //   },
-    //   grid: {
-    //     borderColor: 'var(--fuse-border)',
-    //   },
-    //   labels: this.data.githubIssues.labels,
-    //   legend: {
-    //     show: false,
-    //   },
-    //   plotOptions: {
-    //     bar: {
-    //       columnWidth: '50%',
-    //     },
-    //   },
-    //   series: this.data.githubIssues.series,
-    //   states: {
-    //     hover: {
-    //       filter: {
-    //         type: 'darken',
-    //         value: 0.75,
-    //       },
-    //     },
-    //   },
-    //   stroke: {
-    //     width: [3, 0],
-    //   },
-    //   tooltip: {
-    //     followCursor: true,
-    //     theme: 'dark',
-    //   },
-    //   xaxis: {
-    //     axisBorder: {
-    //       show: false,
-    //     },
-    //     axisTicks: {
-    //       color: 'var(--fuse-border)',
-    //     },
-    //     labels: {
-    //       style: {
-    //         colors: 'var(--fuse-text-secondary)',
-    //       },
-    //     },
-    //     tooltip: {
-    //       enabled: false,
-    //     },
-    //   },
-    //   yaxis: {
-    //     labels: {
-    //       offsetX: -16,
-    //       style: {
-    //         colors: 'var(--fuse-text-secondary)',
-    //       },
-    //     },
-    //   },
-    // };
-    // console.log("chartGithubIssues", this.chartGithubIssues);
-
-    //// Task distribution
-    // this.chartTaskDistribution = {
-    //   chart: {
-    //     fontFamily: 'inherit',
-    //     foreColor: 'inherit',
-    //     height: '100%',
-    //     type: 'polarArea',
-    //     toolbar: {
-    //       show: false,
-    //     },
-    //     zoom: {
-    //       enabled: false,
-    //     },
-    //   },
-    //   labels: this.data.taskDistribution.labels,
-    //   legend: {
-    //     position: 'bottom',
-    //   },
-    //   plotOptions: {
-    //     polarArea: {
-    //       spokes: {
-    //         connectorColors: 'var(--fuse-border)',
-    //       },
-    //       rings: {
-    //         strokeColor: 'var(--fuse-border)',
-    //       },
-    //     },
-    //   },
-    //   series: this.data.taskDistribution.series,
-    //   states: {
-    //     hover: {
-    //       filter: {
-    //         type: 'darken',
-    //         value: 0.75,
-    //       },
-    //     },
-    //   },
-    //   stroke: {
-    //     width: 2,
-    //   },
-    //   theme: {
-    //     monochrome: {
-    //       enabled: true,
-    //       color: '#93C5FD',
-    //       shadeIntensity: 0.75,
-    //       shadeTo: 'dark',
-    //     },
-    //   },
-    //   tooltip: {
-    //     followCursor: true,
-    //     theme: 'dark',
-    //   },
-    //   yaxis: {
-    //     labels: {
-    //       style: {
-    //         colors: 'var(--fuse-text-secondary)',
-    //       },
-    //     },
-    //   },
-    // };
-
-    //// Budget distribution
-    // this.chartBudgetDistribution = {
-    //   chart: {
-    //     fontFamily: 'inherit',
-    //     foreColor: 'inherit',
-    //     height: '100%',
-    //     type: 'radar',
-    //     sparkline: {
-    //       enabled: true,
-    //     },
-    //   },
-    //   colors: ['#818CF8'],
-    //   dataLabels: {
-    //     enabled: true,
-    //     formatter: (val: number): string | number => `${val}%`,
-    //     textAnchor: 'start',
-    //     style: {
-    //       fontSize: '13px',
-    //       fontWeight: 500,
-    //     },
-    //     background: {
-    //       borderWidth: 0,
-    //       padding: 4,
-    //     },
-    //     offsetY: -15,
-    //   },
-    //   markers: {
-    //     strokeColors: '#818CF8',
-    //     strokeWidth: 4,
-    //   },
-    //   plotOptions: {
-    //     radar: {
-    //       polygons: {
-    //         strokeColors: 'var(--fuse-border)',
-    //         connectorColors: 'var(--fuse-border)',
-    //       },
-    //     },
-    //   },
-    //   series: this.data.budgetDistribution.series,
-    //   stroke: {
-    //     width: 2,
-    //   },
-    //   tooltip: {
-    //     theme: 'dark',
-    //     y: {
-    //       formatter: (val: number): string => `${val}%`,
-    //     },
-    //   },
-    //   xaxis: {
-    //     labels: {
-    //       show: true,
-    //       style: {
-    //         fontSize: '12px',
-    //         fontWeight: '500',
-    //       },
-    //     },
-    //     categories: this.data.budgetDistribution.categories,
-    //   },
-    //   yaxis: {
-    //     max: (max: number): number => parseInt((max + 10).toFixed(0), 10),
-    //     tickAmount: 7,
-    //   },
-    // };
-
-    // //Weekly expenses
-    //   this.chartWeeklyExpenses = {
-    //     chart: {
-    //       animations: {
-    //         enabled: false,
-    //       },
-    //       fontFamily: 'inherit',
-    //       foreColor: 'inherit',
-    //       height: '100%',
-    //       type: 'line',
-    //       sparkline: {
-    //         enabled: true,
-    //       },
-    //     },
-    //     colors: ['#22D3EE'],
-    //     series: this.data.weeklyExpenses.series,
-    //     stroke: {
-    //       curve: 'smooth',
-    //     },
-    //     tooltip: {
-    //       theme: 'dark',
-    //     },
-    //     xaxis: {
-    //       type: 'category',
-    //       categories: this.data.weeklyExpenses.labels,
-    //     },
-    //     yaxis: {
-    //       labels: {
-    //         formatter: (val): string => `$${val}`,
-    //       },
-    //     },
-    //   };
-
-    //   // Monthly expenses
-    //   this.chartMonthlyExpenses = {
-    //     chart: {
-    //       animations: {
-    //         enabled: false,
-    //       },
-    //       fontFamily: 'inherit',
-    //       foreColor: 'inherit',
-    //       height: '100%',
-    //       type: 'line',
-    //       sparkline: {
-    //         enabled: true,
-    //       },
-    //     },
-    //     colors: ['#4ADE80'],
-    //     series: this.data.monthlyExpenses.series,
-    //     stroke: {
-    //       curve: 'smooth',
-    //     },
-    //     tooltip: {
-    //       theme: 'dark',
-    //     },
-    //     xaxis: {
-    //       type: 'category',
-    //       categories: this.data.monthlyExpenses.labels,
-    //     },
-    //     yaxis: {
-    //       labels: {
-    //         formatter: (val): string => `$${val}`,
-    //       },
-    //     },
-    //   };
-
-    //   // Yearly expenses
-    //   this.chartYearlyExpenses = {
-    //     chart: {
-    //       animations: {
-    //         enabled: false,
-    //       },
-    //       fontFamily: 'inherit',
-    //       foreColor: 'inherit',
-    //       height: '100%',
-    //       type: 'line',
-    //       sparkline: {
-    //         enabled: true,
-    //       },
-    //     },
-    //     colors: ['#FB7185'],
-    //     series: this.data.yearlyExpenses.series,
-    //     stroke: {
-    //       curve: 'smooth',
-    //     },
-    //     tooltip: {
-    //       theme: 'dark',
-    //     },
-    //     xaxis: {
-    //       type: 'category',
-    //       categories: this.data.yearlyExpenses.labels,
-    //     },
-    //     yaxis: {
-    //       labels: {
-    //         formatter: (val): string => `$${val}`,
-    //       },
-    //     },
-    //   };
-    // }
+ 
   }
 }
