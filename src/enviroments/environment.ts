3d0bc2f2879c1640f9fe5ffb71792b5c194fecbd
Feigi-S <feigi7063@gmail.com>
2024-08-04 14:04:21 +0300

    update nav reload

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: false,
   // apiUrl: 'https://localhost:7222/',
   apiUrl: 'https://miri-rizel-server-nw23.onrender.com/',
-  CLIENT_ID: "592088999303-njnsom6mvdcn0kpesi2ibtlhmblcfcda.apps.googleusercontent.com",
-  API_KEY: "AIzaSyAe4wCi-LL97X_H7fhfaYnB_0cPqfdYVNU",
+  // CLIENT_ID: "592088999303-njnsom6mvdcn0kpesi2ibtlhmblcfcda.apps.googleusercontent.com",
+  // API_KEY: "AIzaSyAe4wCi-LL97X_H7fhfaYnB_0cPqfdYVNU",
+  CLIENT_ID :'797505767772-qhvpc3609kvs82ul23216abgfs3vnncj.apps.googleusercontent.com',
+  API_KEY : 'AIzaSyBF8YlKZHg_lo49Y7mmNV1tD8D8mlI-Gb4'
 };
